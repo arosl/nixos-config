94683e52eeeaa64b77381e52c9acbe32dbea377b
Andreas <andreas@ros.land>
2024-11-25 01:15:31 -0500

    stick to 6.11 until nvidia bug is sorted

--- common-modules/hardware/kernel.nix
+++ common-modules/hardware/kernel.nix
@@ -4,7 +4,8 @@
   pkgs,
   ...
 }: {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_11;
   
   # Kernel modules
   boot.kernelModules = ["i2c-dev" "i2c-piix4" "cpufreq_powersave"];
