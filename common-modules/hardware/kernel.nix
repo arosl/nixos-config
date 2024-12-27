131ca67c10713ba5d3e802db5b67b3f59eb2d18c
Andreas <andreas@ros.land>
2024-12-27 13:17:28 -0500

    system update

--- common-modules/hardware/kernel.nix
+++ common-modules/hardware/kernel.nix
@@ -4,8 +4,8 @@
   pkgs,
   ...
 }: {
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_11;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_11;
   
   # Kernel modules
   boot.kernelModules = ["i2c-dev" "i2c-piix4" "cpufreq_powersave"];
