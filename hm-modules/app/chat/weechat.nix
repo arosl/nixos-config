298cab2c34e86c7c5f81e1e40fdda3fa714b0002
Andreas <andreas@ros.land>
2025-01-05 03:13:45 -0500

    remove outdated packages

--- hm-modules/app/chat/weechat.nix
+++ hm-modules/app/chat/weechat.nix
@@ -1,23 +1,19 @@
 {
-  config,
   pkgs,
-  lib,
   ...
 }: {
   nixpkgs.overlays = [
     (
       self: super: {
         weechat = super.weechat.override {
-          configure = {availablePlugins, ...}: {
+          configure = {...}: {
             scripts = with super.weechatScripts; [
               colorize_nicks
               url_hint
               weechat-autosort
               weechat-go
               weechat-grep
-              weechat-matrix
               weechat-notify-send
-              # weechat-otr
             ];
           };
         };
